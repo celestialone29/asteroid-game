0a57076641fbfb280e81e96431e36d7fd0ddc413
celestialone <fullfiguredvixen29@gmail.com>
2024-11-12 19:07:47 -0500

    Initial commit of asteroid game project

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
-import pygame  # import pygame library
-from constants import *  # import predefined constants
+import sys
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
-    pygame.init()  # initialize pygame
-
-    # Create a new GUI window with the specified screen width and height
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    # Start the game loop
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        # Check for events, including the quit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return  # exit the game loop and end the program
+                return
 
-        # Fill the screen with black
-        screen.fill((0, 0, 0))
+        for obj in updatable:
+            obj.update(dt)
 
-        # Refresh the display
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-# Ensure main() runs only when this file is executed directly
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
 
